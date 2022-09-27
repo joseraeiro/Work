666b36b74b85dafdb153e3b44d2640fae91bcb98
joseraeiro <zeraeiro@gmail.com>
2022-09-27 02:23:25 +0100

    Update pyVirusTotalFileUpload.py

--- pyVirusTotalFileUpload.py
+++ pyVirusTotalFileUpload.py
@@ -23,4 +23,7 @@ for i in res:
         print(i)
         analysis = client.scan_file(f, wait_for_completion=True)
         result =  analysis.to_dict() 
-        print(result)
+        print("Number of malicious matches")
+	    print(result['malicious'])
+        print("Number of undetected matches")
+	    print(result['undetected'])
