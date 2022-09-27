975462a779ff7db45176fcff8df379780326116f
joseraeiro <zeraeiro@gmail.com>
2022-09-27 01:38:05 +0100

    Add files via upload

---
+++ pyVirusTotalFileUpload.py
@@ -0,0 +1,26 @@
+import vt
+import os
+
+client = vt.Client("[virus total API key]")
+
+
+
+# folder path
+dir_path = r'/path/to/files'
+
+# list to store files
+res = []
+
+# Iterate directory
+for path in os.listdir(dir_path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dir_path, path)):
+        res.append(path)
+
+
+for i in res:
+    with open(i, "rb") as f:
+        print(i)
+        analysis = client.scan_file(f, wait_for_completion=True)
+        result =  analysis.to_dict() 
+        print(result)
