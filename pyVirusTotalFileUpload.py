896be47362ce62f21e86777745c4f8b98881e2ab
joseraeiro <zeraeiro@gmail.com>
2022-09-27 12:01:54 +0100

    Update pyVirusTotalFileUpload.py

--- pyVirusTotalFileUpload.py
+++ pyVirusTotalFileUpload.py
@@ -1,26 +1,31 @@
 import vt
 import os
 
-client = vt.Client("[virus total API key]")
+client = vt.Client("[VirusTotal API key]")
 
+dir_path = r'path/to/files'
 
-
-# folder path
-dir_path = r'/path/to/files'
-
-# list to store files
 res = []
 
-# Iterate directory
 for path in os.listdir(dir_path):
     # check if current path is a file
     if os.path.isfile(os.path.join(dir_path, path)):
         res.append(path)
 
-
 for i in res:
     with open(i, "rb") as f:
         print(i)
         analysis = client.scan_file(f, wait_for_completion=True)
-        result =  analysis.to_dict() 
-        print(result)
+        result =  analysis.to_dict()
+        filtered_list = ['malicious', 'undetected']
+        def seek_keys(d, key_list):
+            for k, v in d.items():
+                if k in key_list:
+                    if isinstance(v, dict):
+                        print(k + ": " + list(v.keys())[0])
+                    else:
+                        print(k + ": " + str(v))
+                if isinstance(v, dict):
+                    seek_keys(v, key_list)
+
+    (seek_keys(result, filtered_list))
